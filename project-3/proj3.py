245b6c79a197c5275edd75ec18d1833138d77133
mschrader15 <mcschrader@crimson.ua.edu>
2021-10-14 21:13:41 -0500

    updated jupyter notebook

--- project-3/proj3.py
+++ project-3/proj3.py
@@ -42,23 +42,16 @@ class Plane:
             [0, 0, 0, 0],
         ])
 
-        self.Q = np.eye(self.A.shape[0])
-
-        self.R = np.eye(self.B.shape[1]) * 0.01
-
-        self.u_bar = 250  # ft/s
-
         self.x0 = np.array(
             [250, 0, 0, 0, 0, 0, 0, 0]
         )
 
-    def optimal_f(self, time_vector: np.ndarray, target_states: List[np.ndarray], Q: np.ndarray, R: np.ndarray) -> object:
+    def optimal_f(self, time_vector: np.ndarray, target_states: List[np.ndarray], Q: np.ndarray, R: np.ndarray, control_index: tuple) -> object:
         
         A = np.r_[np.c_[self.A, np.zeros(self.A.shape[0])], [[1, 0, 0, 0, 0, 0, 0, 0, 0]]]
         B = np.r_[self.B, [[0] * self.B.shape[1]]]
-#         C = np.eye(A.shape[0])
-#         K, _, _ = lqr(A, B, Q, self.R)
         K = self.design_lrq(A, B, Q, R)
+        C = np.diag([-1 if i in control_index else 0 for i in range(self.A.shape[0])] + [0])
 
         if target_states[0].shape[0] != B.shape[0]:
             target_states = [np.append(_t, [0]) for _t in target_states]
@@ -72,13 +65,10 @@ class Plane:
 
         def f(t, x_t, ):
             wr = t_f(t)
-            return np.array(((Aaug @ (x_t - wr).T) - (wr - X0)))[0]
+            return np.array(((Aaug @ (x_t - wr).T) + C @ (wr - X0)))[0]
         
         return f
 
-    def augA(self, ) -> np.ndarray:
-        self.A
-    
     @property
     def ss(self, ) -> StateSpace:
         return StateSpace(self.A, self.B, np.eye(self.A.shape[0]), np.zeros(self.B.shape))
@@ -87,7 +77,6 @@ class Plane:
         self.T = t_vect
         self.U = u_vect
         return solve_ivp(self._f(), y0=self.x0, t_span=(t_vect[0], t_vect[-1]), max_step=Plane.MAX_STEP, args=(None, ))
-#         return lsim(self.ss, u_vect, t_vect, self.x0)
 
     def simulate_controlled(self, time_vector: np.ndarray, *args, **kwargs) -> Tuple[np.ndarray]:
         # this is a bad practive but getting lazy
@@ -96,8 +85,8 @@ class Plane:
 
     def _f(self, ):
         def f(t, x_t, wr):
-            U = np.array(-1 * self.K @ (x_t - wr))[0] if wr is not None else np.array(self.step_function(t))
-            return self.A @ x_t + (self.B @ U.T)
+            U = np.array(self.step_function(t))
+            return self.A @ (x_t - self.x0) + (self.B @ U.T)
         return f
 
     def step_function(self, t: float):
@@ -112,11 +101,12 @@ class Plane:
 
         # compute the LQR gain
         K = np.matrix(la.inv(R) @ (B.T@ X))
-
+        
+        # find the eigne values
         eigVals, eigVecs = la.eig(A-B*K)
 
         return K
-
+    
     def control_lqr(self, ) -> np.ndarray:
         # This function is used as a check of my own LQR implementation
         from control import lqr
@@ -124,8 +114,8 @@ class Plane:
         return lqr(self.ss, self.Q, self.R)
 
     @staticmethod
-    def plot(t: np.ndarray, y: np.ndarray, u: np.ndarray, flat: bool = True, target: tuple = None) -> None:
-        fig = make_subplots(rows=5,
+    def plot(t: np.ndarray, y: np.ndarray, u: np.ndarray, flat: bool = True, target: tuple = None, input_: bool = True) -> None:
+        fig = make_subplots(rows=5 if input_ else 3,
                             cols=1,
                             shared_xaxes=True,
                             vertical_spacing=0.05,
@@ -145,40 +135,40 @@ class Plane:
              [u, "lbs", [["thrust", 1]]],
              ]
         ):
-            for name, ind in inner_settings:
-                fig.add_trace(go.Scatter(
-                    x=t,
-                    #                     y=[val[ind] + self.u_bar if j < 1 else val[ind] for val in data],
-                    y=data[ind] if flat else [val[ind] for val in data],
-                    name=name
-                ),
-                    row=j + 1, col=1
-                )
-                
-                if target and j < 3:
-                    fig.add_trace(
-                        go.Scatter(
-                        x=target[0],
-                        y=[val[ind] for val in target[1]],
-                        line_color="black",
-                        line_dash="dash",
-                        name="Target",
-                        showlegend=j < 1 
+            if input_ or j < 3: 
+                for name, ind in inner_settings:
+                    fig.add_trace(go.Scatter(
+                        x=t,
+                        #                     y=[val[ind] + self.u_bar if j < 1 else val[ind] for val in data],
+                        y=data[ind] if flat else [val[ind] for val in data],
+                        name=name
                     ),
-                    row=j + 1, 
-                    col=1,
-                )
+                        row=j + 1, col=1
+                    )
 
-            update_dict = {
-                f"yaxis{j+1}" if j > 0 else "yaxis": dict(title=yaxis_title)
-            }
+                    if target and j < 3:
+                        fig.add_trace(
+                            go.Scatter(
+                            x=target[0],
+                            y=[val[ind] for val in target[1]],
+                            line_color="black",
+                            line_dash="dash",
+                            name="Target",
+                            showlegend=j < 1 
+                        ),
+                        row=j + 1, 
+                        col=1,
+                    )
+
+                update_dict = {
+                    f"yaxis{j+1}" if j > 0 else "yaxis": dict(title=yaxis_title)
+                }
             fig.update_layout(update_dict)
         fig.update_layout(
             height=800
         )
 
         fig.show()
-
 
 
 if __name__ == "__main__":
@@ -189,7 +179,7 @@ if __name__ == "__main__":
 
     # K = p.design_lrq()
     res = p.simulate_controlled(time_vector=np.linspace(0, 20, 100), target_states=[np.array([250, 0, 0, 0, 0, 0, 0, 0])] * 20 +  [np.array([260, 0, 0, 0, 0, 0, 0, 0])] * 80, 
-    Q=np.diag([10] * 9), R=np.diag([20, 20, 20, 20]))
+    Q=np.diag([1, ] * 9), R=np.diag([20, 20, 20, 20]), control_index=(0, 3))
     # print(res)
 
     # u = np.array([[0, 0, 0, 0]] * 10 + [[0, 10, 0, 0]] * 90)
