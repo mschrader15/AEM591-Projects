bf8564874253cc1a5d2f48e24128f66ec8467a4b
Max Schrader <mcschrader@crimson.ua.edu>
2021-10-21 21:35:32 -0500

    HW finished except for the last comment

--- project-4/src.py
+++ project-4/src.py
@@ -129,6 +129,7 @@ class EKFStep:
     x_k_k: np.ndarray
     y_k: np.ndarray
     P_k_k: np.ndarray
+    P_k_k_1: np.ndarray
     S_k: np.ndarray
 
 
@@ -203,7 +204,7 @@ class EKF(LTI):
         self.P_posteriori = P_k_k.copy()
 
         # return the states that we want to plot
-        return EKFStep(x_k_k=x_k_k, y_k=y_k, P_k_k=P_k_k, S_k=S_k)
+        return EKFStep(x_k_k=x_k_k, y_k=y_k, P_k_k=P_k_k, S_k=S_k, P_k_k_1=P_priori)
 
     def run(self, mode="") -> List[EKFStep]:
 
@@ -225,6 +226,60 @@ class EKF(LTI):
             i += 1
 
         return results
+
+
+def confidence_ellipse(x, y, cov, n_std=1.96, size=100):
+    """
+    Largely from https://gist.github.com/dpfoose/38ca2f5aee2aea175ecc6e599ca6e973
+
+    Get the covariance confidence ellipse of *x* and *y*.
+    Parameters
+    ----------
+    x, y : array-like, shape (n, )
+        Input data.
+    n_std : float
+        The number of standard deviations to determine the ellipse's radiuses.
+    size : int
+        Number of points defining the ellipse
+    Returns
+    -------
+    String containing an SVG path for the ellipse
+    
+    References (H/T)
+    ----------------
+    https://matplotlib.org/3.1.1/gallery/statistics/confidence_ellipse.html
+    https://community.plotly.com/t/arc-shape-with-path/7205/5
+    """
+    pearson = cov[0, 1] / np.sqrt(cov[0, 0] * cov[1, 1])
+    # Using a special case to obtain the eigenvalues of this
+    # two-dimensionl dataset.
+    ell_radius_x = np.sqrt(1 + pearson)
+    ell_radius_y = np.sqrt(1 - pearson)
+    theta = np.linspace(0, 2 * np.pi, size)
+    ellipse_coords = np.column_stack([ell_radius_x * np.cos(theta), ell_radius_y * np.sin(theta)])
+    
+    # Calculating the stdandard deviation of x from
+    # the squareroot of the variance and multiplying
+    # with the given number of standard deviations.
+    x_scale = np.sqrt(cov[0, 0]) * n_std
+    x_mean = np.mean(x)
+
+    # calculating the stdandard deviation of y ...
+    y_scale = np.sqrt(cov[1, 1]) * n_std
+    y_mean = np.mean(y)
+  
+    translation_matrix = np.tile([x_mean, y_mean], (ellipse_coords.shape[0], 1))
+    rotation_matrix = np.array([[np.cos(np.pi / 4), np.sin(np.pi / 4)],
+                                [-np.sin(np.pi / 4), np.cos(np.pi / 4)]])
+    scale_matrix = np.array([[x_scale, 0],
+                            [0, y_scale]])
+    ellipse_coords = ellipse_coords.dot(rotation_matrix).dot(scale_matrix) + translation_matrix
+        
+    path = f'M {ellipse_coords[0, 0]}, {ellipse_coords[0, 1]}'
+    for k in range(1, len(ellipse_coords)):
+        path += f'L{ellipse_coords[k, 0]}, {ellipse_coords[k, 1]}'
+    path += ' Z'
+    return path
 
 
 if __name__ == "__main__":
